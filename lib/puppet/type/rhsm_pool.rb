9cb0f78ca85baecefe2c61601379ba4c11e70df7
JD Powell <waveclaw@hotmail.com>
2015-08-25 01:12:35 -0500

    Update documentation of rhsm_pool type

--- lib/puppet/type/rhsm_pool.rb
+++ lib/puppet/type/rhsm_pool.rb
@@ -2,12 +2,12 @@ require 'puppet/property/boolean'
 require 'puppet/type'
 
 Puppet::Type.newtype(:rhsm_pool) do
-  @doc = ""
+  @doc = "Abstract the concept of an Entitlement Pool from which active subscriptions can be drawn"
 
   ensurable
 
   newparam(:id, :namevar => true) do
-    desc "The rhsm channel to subscribe to."
+    desc "An Entitlement Pool to which the server is subscribed"
   end
 
 end
