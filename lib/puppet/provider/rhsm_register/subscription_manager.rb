66c55fca0d2fb44ef889795eab656b9ef95f34e4
Jeremiah D. Powell <waveclaw@waveclaw.com>
2015-09-19 16:20:53 -0500

    also check resource defaults

--- lib/puppet/provider/rhsm_register/subscription_manager.rb
+++ lib/puppet/provider/rhsm_register/subscription_manager.rb
@@ -78,7 +78,8 @@ Puppet::Type.type(:rhsm_register).provide(:subscription_manager) do
         subscription_attach
       else
         # trying to re-register
-        if @property_hash[:force].nil? or @property_hash[:force] == false
+        if (@property_hash[:force].nil? or @property_hash[:force] == false) and
+           (@resource[:force].nil? or @resource[:force] == faluse)
               self.fail("Require force => true to register already registered server")
         end
         register
