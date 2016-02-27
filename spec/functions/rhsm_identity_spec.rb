6108dbfba330f0a8823337a099d5574b19a8f0fc
Jeremiah D. Powell <waveclaw@waveclaw.com>
2016-02-27 13:01:39 -0600

    update identity test for actual fact

--- spec/functions/rhsm_identity_spec.rb
+++ spec/functions/rhsm_identity_spec.rb
@@ -19,44 +19,26 @@ org ID: default-org
 environment name: Library
 EOD
 
-awk='/usr/bin/awk'
-sm='/bin/sm'
 
 describe Facter::Util::Rhsm_identity, :type => :puppet_function do
   it "should return the expected data for old style return" do
-#    expect(Facter::Util::Resolution).to receive(:exec).
-#      with("/usr/bin/which awk") { awk }
     expect(Facter::Util::Resolution).to receive(:exec).
-      with('/usr/bin/which subscription-manager') { sm }
-    expect(Facter::Util::Resolution).to receive(:exec).
-      with("/bin/sm identity") { raw_data1 }
+      with("/usr/sbin/subscription-manager identity") { raw_data1 }
     expect(Facter::Util::Rhsm_identity.rhsm_identity).to eq(expected_data)
   end
   it "should return the expected data for new style" do
-#    expect(Facter::Util::Resolution).to receive(:exec).
-#      with("/usr/bin/which awk") { awk }
     expect(Facter::Util::Resolution).to receive(:exec).
-      with('/usr/bin/which subscription-manager') { sm }
-    expect(Facter::Util::Resolution).to receive(:exec).
-      with("/bin/sm identity") { raw_data2 }
+      with("/usr/sbin/subscription-manager identity") { raw_data2 }
     expect(Facter::Util::Rhsm_identity.rhsm_identity).to eq(expected_data)
   end
   it "should return the nothing for no data" do
-#    expect(Facter::Util::Resolution).to receive(:exec).
-#      with("/usr/bin/which awk") { awk }
     expect(Facter::Util::Resolution).to receive(:exec).
-      with('/usr/bin/which subscription-manager') { sm }
-    expect(Facter::Util::Resolution).to receive(:exec).
-      with("#{sm} identity") { '' }
+      with("/usr/sbin/subscription-manager identity") { '' }
     expect(Facter::Util::Rhsm_identity.rhsm_identity).to eq(nil)
   end
   it "should return the nothing for no command" do
-#    expect(Facter::Util::Resolution).to receive(:exec).
-#      with("/usr/bin/which awk") { awk }
     expect(Facter::Util::Resolution).to receive(:exec).
-      with('/usr/bin/which subscription-manager') { sm }
-    expect(Facter::Util::Resolution).to receive(:exec).
-      with("#{sm} identity") { throw Error }
+      with("/usr/sbin/subscription-manager identity") { throw Error }
     expect(Facter::Util::Rhsm_identity.rhsm_identity).to eq(nil)
   end
 end
