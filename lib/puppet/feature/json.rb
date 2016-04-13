8438a1dbfa2a7820dd9627d017a9da6bb2ef50f5
Jeremiah D. Powell <waveclaw@waveclaw.com>
2016-04-12 23:57:56 -0500

    document the json feature

--- lib/puppet/feature/json.rb
+++ lib/puppet/feature/json.rb
@@ -1,1 +1,13 @@
+#!/usr/bin/ruby
+#
+# Include a json implementation in Puppet
+#
+# === Authors
+#
+# Gaël Chamoulaud <gchamoul@redhat.com>
+#
+# === Copyright
+#
+# Copyright 2015 Gaël Chamoulaud <gchamoul@redhat.com>
+#
 Puppet.features.add(:json, :libs => ["json"])
