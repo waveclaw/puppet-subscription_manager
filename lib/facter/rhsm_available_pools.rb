0b3922b092a558fb84d6e56dc4ae599cff3a0f53
Jeremiah D. Powell <waveclaw@waveclaw.com>
2016-05-11 20:48:43 -0500

    class/module name must be CONSTANT

--- lib/facter/rhsm_available_pools.rb
+++ lib/facter/rhsm_available_pools.rb
@@ -9,7 +9,7 @@
 #
 require 'facter/util/cacheable'
 
-module Facter::Util::rhsm_available_pools
+module Facter::Util::Rhsm_available_pools
   @doc=<<EOF
   Available Subscription Pools for this client.
 EOF
