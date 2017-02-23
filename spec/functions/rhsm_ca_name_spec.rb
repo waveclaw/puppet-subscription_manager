b331a9320039fc85b5b60b979c23aa3b700d8081
Jeremiah D. Powell <waveclaw@waveclaw.com>
2017-02-23 14:35:27 -0600

    switch to type fact for fact test

--- spec/functions/rhsm_ca_name_spec.rb
+++ spec/functions/rhsm_ca_name_spec.rb
@@ -17,41 +17,46 @@ cafiles = {
 
 # create fake intermediary objects to get detailed testing in each case
 fakeCert = StringIO.new(" ",'r')
-subject = Class.new do
+cert = Class.new do
   def subject
   end
 end
 
-describe Facter::Util::Rhsm_ca_name, :type => :puppet_function do
+describe Facter::Util::Rhsm_ca_name, :type => :fact do
   shared_examples_for 'on a supported os' do |cafile|
     before :each do
-      allow(File).to receive(:exists?).with(cafile) { true }
+      Facter::Util::Loader.any_instance.stubs(:load_all)
+      Facter.clear
+      Facter.clear_messages
+      allow(File).to receive(:exists?) { false }
     end
     it "should return nothing when there is an error" do
       expect(File).to receive(:exists?).with(cafile) { true }
       expect(File).to receive(:open).with(cafile) { throw Error }
-      expect(Facter::Util::Rhsm_ca_name.rhsm_ca_name(cafile)).to eq(nil)
+      expect(subject.rhsm_ca_name(cafile)).to eq(nil)
     end
     it "should return the expected domain from normal Certificate subjects" do
       expect(File).to receive(:exists?).with(cafile) { true }
       expect(File).to receive(:open).with(cafile) { fakeCert }
-      expect(OpenSSL::X509::Certificate).to receive(:new) { subject }
-      expect(subject).to receive(:subject) {
+      expect(OpenSSL::X509::Certificate).to receive(:new) { cert }
+      expect(cert).to receive(:subject) {
         '/C=US/ST=Main/L=Hobokeen/O=Your Mama/CN=example.net'}
-      expect(Facter::Util::Rhsm_ca_name.rhsm_ca_name(cafile)).to eq('example.net')
+      expect(subject.rhsm_ca_name(cafile)).to eq('example.net')
     end
     it "should return nothing for bad Certificate subjects" do
       expect(File).to receive(:exists?).with(cafile) { true }
       expect(File).to receive(:open).with(cafile) { fakeCert }
-      expect(OpenSSL::X509::Certificate).to receive(:new) { subject }
-      expect(subject).to receive(:subject) { 'random garbage' }
-      expect(Facter::Util::Rhsm_ca_name.rhsm_ca_name(cafile)).to eq(nil)
+      expect(OpenSSL::X509::Certificate).to receive(:new) { cert }
+      expect(cert).to receive(:subject) { 'random garbage' }
+      expect(subject.rhsm_ca_name(cafile)).to eq(nil)
     end
   end
 
   shared_examples_for 'on an unsupported os' do |cafile|
     before :each do
+      Facter::Util::Loader.any_instance.stubs(:load_all)
       Facter.clear
+      Facter.clear_messages
       allow(File).to receive(:exists?) { false }
     end
     it "should return nothing" do
