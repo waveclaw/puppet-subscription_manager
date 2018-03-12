e3454cb66449cc0103606969a54f07f38470ebd6
Jeremiah D. Powell <waveclaw@waveclaw.com>
2018-03-12 13:04:33 +0800

    migrate to PDK 1.4 format

---
+++ spec/spec_helper_local.rb
@@ -0,0 +1,13 @@
+require 'simplecov'
+
+SimpleCov.start do
+   add_filter '/spec/'
+   # Exclude bundled Gems in typical locations
+   add_filter '/.vendor/'
+   add_filter '/.bundle/'
+end
+
+
+RSpec.configure do |c|
+    c.mock_with :rspec
+end
