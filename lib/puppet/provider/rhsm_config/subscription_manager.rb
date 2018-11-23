5e36de2b5492e55ea7cb984182dd34da3fd7d6b9
Jeremiah D. Powell <waveclaw@waveclaw.com>
2018-11-23 00:47:14 -0700

    refactor out the ini parser to dedicated tests, push rubcop fixes

--- lib/puppet/provider/rhsm_config/subscription_manager.rb
+++ lib/puppet/provider/rhsm_config/subscription_manager.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
 #
 #  Provide a mechanism to configure subscription to a katello or Satellite 6
 #  server.
@@ -15,51 +17,53 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
     RedHat Subscription Manager subscription to a Katello or Satellite 6 server.
   EOS
 
-  confine :osfamily => :redhat
+  confine osfamily: :redhat
 
-  commands :subscription_manager => "subscription-manager"
+  commands subscription_manager: 'subscription-manager'
 
   # (Re-)Write the configuration file for subscription-manager.
   #  This controls the target of registration and paths to features.
   def flush
-    Puppet.debug("rhsm.flush: will sync to disk the configuration.")
+    Puppet.debug('rhsm.flush: will sync to disk the configuration.')
     if exists?
-      Puppet.debug("rhsm.flush: This server will be configured for rhsm.")
+      Puppet.debug('rhsm.flush: This server will be configured for rhsm.')
       config = :apply
     else
-      Puppet.debug("rhsm.flush: The configuration will be completely set to default.")
+      Puppet.debug('rhsm.flush: The configuration will be completely set to default.')
       config = :remove
     end
     cmds = build_config_parameters(config)
     if (cmds[:remove]).nil?
-      Puppet.debug("rhsm.flush: given nothing to remove.")
+      Puppet.debug('rhsm.flush: given nothing to remove.')
     else
-      cmds[:remove].each { |parameter|
-        subscription_manager(*['config', parameter])
-      }
+      cmds[:remove].each do |parameter|
+        subscription_manager('config', parameter)
+      end
     end
     if (cmds[:apply]).nil?
-      Puppet.debug("rhsm.flush: given nothing to configure.")
+      Puppet.debug('rhsm.flush: given nothing to configure.')
     else
       subscription_manager(*cmds[:apply])
     end
-    @property_hash = self.class.get_configuration
+    @property_hash = self.class.on_disk_configuration
   end
 
   def create
     # setup properties
     # see https://groups.google.com/forum/#!topic/puppet-users/G3z41gFi0Dk
-       resource.class.text_options.each { |property|
-         if value = resource.should(property)
-           @property_hash[property] = value
-         end
-       }
-       resource.class.binary_options.each { |property|
-         if value = resource.should(property)
-           @property_hash[property] = value
-         end
-       }
-       @property_hash[:ensure] = :present
+    resource.class.text_options.each do |property|
+      value = resource.should(property)
+      unless value.nil?
+        @property_hash[property] = value
+      end
+    end
+    resource.class.binary_options.each do |property|
+      value = resource.should(property)
+      unless value.nil?
+        @property_hash[property] = value
+      end
+    end
+    @property_hash[:ensure] = :present
   end
 
   def destroy
@@ -70,60 +74,62 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
     @property_hash[:ensure] == :present
   end
 
-
   def self.instances
-    config = get_configuration
-    if config.nil? or config == {}
-      [ ]
+    config = on_disk_configuration
+    if config.nil? || config == ({})
+      []
     else
-      [ new(config) ]
+      [new(config)]
     end
   end
 
   def self.prefetch(resources)
-    instances.each { |prov|
-      if resource = resources[prov.name]
-        resource.provider = prov
-      end
-    }
+    x = instances
+    resources.keys.each do |name|
+      provider = x.find { |i| i.name == name }
+      resources[name].provider = provider unless provider.nil?
+    end
   end
-
 
   mk_resource_methods
 
   # Get the on disk config
   # @return [hash] the settings of the configuration and the identity
   # @api private
-  def self.get_configuration
-    Puppet.debug("Will parse the configuration")
+  def self.on_disk_configuration
+    # Puppet.debug('Will parse the configuration')
     conf = {}
-    data = subscription_manager(['config','--list'])
-    #Puppet.debug("Recieved #{data.size} characters of configuration data.")
-    unless data.nil?
-      conf = ini_parse(data)
-      unless (conf.nil? or conf == {})
-        conf[:name] = '/etc/rhsm/rhsm.conf' #Puppet::Type.type(:rhsm_config).$default_filename
-        conf[:provider] = :subscription_manager
-        conf[:ensure] = :present
-        # bypass the config --list command for this since it typically has a variable expansion
-        begin
-          value = nil
-          config = File.open(conf[:name])
-          config.each { |line|
-              if line =~ /^repo_ca_cert = (.+)/
-                value = $1.strip
-              end
-          }
-          config.close()
-          if value
-            conf[:rhsm_repo_ca_cert] = value
-          end
-        rescue Exception => e
-            Puppet.debug("get_configuration failed to parse repo_ca_cert: #{e.to_s}")
-        end
-      end
+    data = subscription_manager(['config', '--list'])
+    conf = ini_parse(data) unless data.nil?
+    unless conf.nil? || conf == ({}) || !conf.is_a?(Hash)
+      conf[:name] = '/etc/rhsm/rhsm.conf' # Puppet::Type.type(:rhsm_config).$default_filename
+      conf[:provider] = :subscription_manager
+      conf[:ensure] = :present
+      conf[:rhsm_repo_ca_cert] = repo_cert(conf[:name])
     end
     conf
+  end
+
+  # Get the rhsm sections ca certificate
+  # @return String the setting string for the CA
+  # @param {String} the filename to pull the name out of
+  # @api private
+  def self.repo_cert(configname)
+    # config --list command typically has a variable expansion for repo_ca_cert
+    value = nil
+    begin
+      on_disk = File.open(configname)
+      on_disk.split("\n").each do |line|
+        m = line.match(%r{[^#]*repo_ca_cert = (.+)})
+        unless m.nil?
+          value = m[1].strip
+        end
+      end
+      on_disk.close
+    rescue StandardError => e
+      Puppet.debug("on_disk_configuration failed to parse repo_ca_cert: #{e}")
+    end
+    value
   end
 
   # return a digit or boolean based on option type
@@ -133,11 +139,11 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
   # @param [digit] the data
   # @api private
   def self.parse_digit(section, title, digit)
-    if Puppet::Type.type(:rhsm_config).binary_options.has_key? "#{section}_#{title}".to_sym
-      value = (digit == '1') ? true : false
-    else
-      value = digit.to_i
-    end
+    value = if Puppet::Type.type(:rhsm_config).binary_options.key? "#{section}_#{title}".to_sym
+              (digit == '1') ? true : false
+            else
+              digit.to_i
+            end
     value
   end
 
@@ -152,12 +158,11 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
   # @return [array] the default list
   # @param [symbol] the default setting parameter to exclude
   # @api private
-  def self.defaults_to=(value)
-    @defaults_to = value
+  class << self
+    attr_writer :defaults_to
   end
 
-
-  # Primitive init parser for the strange output of subscription-manager
+  # Primitive parser for the ini-like output of subscription-manager
   # @return [hash] the parsed configuration data
   # @param [String] the raw data
   # @api private
@@ -166,81 +171,83 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
     output = {}
     title = nil
     section = nil
-    input.split("\n").each { |line|
+    input.split("\n").each do |line|
       # secdions look like ' [abc] '
-      if line =~/^\s*\[(.+)\]/
-        section = $1
+      m = line.match(%r{^\s*\[(.+)\]})
+      unless m.nil?
+        section = m[1]
         next
       end
       # lines can be 'thing = [], thing = value or thing = [value]'
       # 'thing = [value]' is a default, they can either be skipped or noted later
-      if line =~ /\s*([a-z_]+) = (.*)/
-          title = $1
-          raw_value = $2.chomp
-          case raw_value
-          when /\[\]/
-            # if nil is used here then puppet considers parameters set to
-            # '' to be in need of sync at all time
-            value = ''
-            @defaults_to.push "#{section}_#{title}".to_sym
-          when /\[(\d+)\]/
-            @defaults_to.push "#{section}_#{title}".to_sym
-            value = parse_digit(section, title, $1)
-          when /^(\d+)$/
-            value = parse_digit(section, title, $1)
-          when /\[(.+)\]/
-            @defaults_to.push "#{section}_#{title}".to_sym
-            value = $1
-          when /(\S+)/
-            value = $1
-          else
-            # same as above, avoid nil for undefined parameters
-            value = ''
-          end
-          #Puppet.debug("in section #{section} in title #{title} with value #{value}")
-          unless value.nil? or section.nil? or title.nil?
-            output["#{section}_#{title}".to_sym] = value
-          end
-        next
+      m = line.match(%r{^\s*([a-z_]+) = (.*)})
+      unless m.nil?
+        title = m[1]
+        raw_value = m[2]
+        case raw_value
+        when %r{\[\]}
+          # if nil is used here then puppet considers parameters set to
+          # '' to be in need of sync at all time
+          value = ''
+          @defaults_to.push "#{section}_#{title}".to_sym
+        when %r{\[(\d+)\]}
+          @defaults_to.push "#{section}_#{title}".to_sym
+          value = parse_digit(section, title, Regexp.last_match(1))
+        when %r{^(\d+)$}
+          value = parse_digit(section, title, Regexp.last_match(1))
+        when %r{\[(.+)\]}
+          @defaults_to.push "#{section}_#{title}".to_sym
+          value = Regexp.last_match(1)
+        when %r{(^[a-zA-Z_. /]+$)}
+          value = Regexp.last_match(1)
+        when %r{(\S+)}
+          value = Regexp.last_match(1)
+        else
+          # same as above, avoid nil for undefined parameters
+          value = ''
+        end
       end
-    }
-    #Puppet.debug("Parsed out #{output.size} lines of data")
+      # Puppet.debug("in section #{section} in title #{title} with value #{value}")
+      unless value.nil? || section.nil? || title.nil?
+        output["#{section}_#{title}".to_sym] = value
+      end
+      next
+    end
+    # Puppet.debug("Parsed out #{output.size} lines of data")
     output
   end
 
-    # Build a config option string
-    # @return [Hash(Array(String))] the options for a config command
-    #  split into :apply array of setter options and :remove array of removals
-    # @api private
-    def build_config_parameters(config)
-      setparams = [ "config" ]
-      removeparams = [ ]
-      # only set non-empty non-equal values
-      @property_hash.keys.each { |key|
-        # skip meta parameters and default values
-        unless [ :ensure, :title,  :tags, :name, :provider].include? key or
-         (!@defaults_to.nil? and @defaults_to.include? key)
-          section = key.to_s.sub('_','.')
-          if config == :remove or
-            (@property_hash[key] == '' and @property_hash[key] != @resource[key]) or
-            (@property_hash[key] == nil and @property_hash[key] != @resource[key])
-            removeparams << "--remove=#{section}"
-          elsif config == :apply and (@property_hash[key] != '')
-            setparams << "--#{section}"
-          end
-          if @resource.class.binary_options.has_key? key and @property_hash[key] != ''
-            value = (@property_hash[key] == true ) ? 1 : 0
-          else
-            value = @property_hash[key]
-          end
-          unless config == :remove or @property_hash[key] == '' or value == '' or value.nil?
-             setparams << value.to_s
-          end
-        end
-      }
-      setparams = nil if setparams == ['config']
-      removeparams = nil if removeparams == []
-      {:apply => setparams, :remove => removeparams}
+  # Build a config option string
+  # @return [Hash(Array(String))] the options for a config command
+  #  split into :apply array of setter options and :remove array of removals
+  # @api private
+  def build_config_parameters(config)
+    setparams = ['config']
+    removeparams = []
+    # only set non-empty non-equal values
+    @property_hash.keys.each do |key|
+      # skip meta parameters and default values
+      next if [:ensure, :title, :tags, :name, :provider].include?(key) ||
+              (!@defaults_to.nil? && @defaults_to.include?(key))
+      section = key.to_s.sub('_', '.')
+      if config == :remove ||
+         (@property_hash[key] == '' && @property_hash[key] != (@resource[key])) ||
+         (@property_hash[key].nil? && @property_hash[key] != (@resource[key]))
+        removeparams << "--remove=#{section}"
+      elsif config == :apply && (@property_hash[key] != '')
+        setparams << "--#{section}"
+      end
+      value = if @resource.class.binary_options.key?(key) && @property_hash[key] != ''
+                (@property_hash[key] == true) ? 1 : 0
+              else
+                @property_hash[key]
+              end
+      unless config == :remove || @property_hash[key] == '' || value == '' || value.nil?
+        setparams << value.to_s
+      end
     end
-
+    setparams = nil if setparams == ['config']
+    removeparams = nil if removeparams == []
+    { apply: setparams, remove: removeparams }
+  end
 end
--- spec/unit/provider/rhsm_config/subscription_manager_spec.rb
+++ spec/unit/provider/rhsm_config/subscription_manager_spec.rb
@@ -78,6 +78,30 @@ describe  provider_class, '#rhsm_config.provider' do
 [] - Default value in use
 EOD
 
+  raw_values = {
+    server_hostname: 'katello.example.com',
+    server_insecure: false,
+    server_port: 443,
+    server_prefix: '/rhsm',
+    server_proxy_hostname: 'proxy.example.com',
+    server_proxy_password: 'proxy_password',
+    server_proxy_port: 4443,
+    server_proxy_user: 'proxy_user',
+    server_ssl_verify_depth: 3,
+    rhsm_baseurl: 'https://katello.example.com/pulp/repos',
+    rhsm_ca_cert_dir: '/etc/rhsm/ca/',
+    rhsm_consumercertdir: '/etc/pki/consumer',
+    rhsm_entitlementcertdir: '/etc/pki/entitlement',
+    rhsm_full_refresh_on_yum: true,
+    rhsm_manage_repos: true,
+    rhsm_pluginconfdir: '/etc/rhsm/pluginconf.d',
+    rhsm_plugindir: '/usr/share/rhsm-plugins',
+    rhsm_productcertdir: '/etc/pki/product',
+    rhsm_repo_ca_cert: '/etc/rhsm/ca/',
+    rhsm_report_package_profile: true,
+    rhsmcertd_autoattachinterval: 1440
+  }
+
   raw_hostname01_data = <<-EOD
  [server]
     hostname = katello01.example.com
@@ -108,6 +132,11 @@ EOD
 
 [] - Default value in use
 EOD
+
+  raw_hostname01_values = {
+    server_hostname: 'katello01.example.com',
+    rhsm_baseurl: 'https://katello01.example.com/pulp/repos'
+  }
 
   let(:resource) do
     Puppet::Type.type(:rhsm_config).new(properties)
@@ -144,7 +173,7 @@ EOD
     context 'for expected values of properties' do
       properties.keys.each do |key|
         it "should be able to set the #{key} property" do
-          expect(provider.class).to receive(:get_configuration).and_return(
+          expect(provider.class).to receive(:on_disk_configuration).and_return(
             name: title, provider: provider, ensure: :present,
           )
           configs = provider.class.instances
@@ -165,7 +194,7 @@ EOD
   describe 'self.prefetch' do
     it { expect(provider.class).to respond_to(:prefetch) }
     it 'can be called on the provider' do
-      expect(provider.class).to receive(:get_configuration).and_return(properties)
+      expect(provider.class).to receive(:on_disk_configuration).and_return(properties)
       expect(provider.class.prefetch(properties)).to be_truthy
     end
   end
@@ -196,7 +225,7 @@ EOD
       res.provider.set(server_port: 443)
       expect(res.provider).to receive(:build_config_parameters).and_return(apply: ['foo'], remove: nil)
       expect(res.provider).to receive(:subscription_manager).with('foo')
-      expect(res.provider.class).to receive(:subscription_manager).with(['config', '--list']) {}
+      expect(res.provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(nil)
       allow(res.provider).to receive(:exists?).and_return(true)
       res.provider.flush
     end
@@ -217,59 +246,76 @@ EOD
     end
   end
 
-  describe 'get_configuration' do
+  #
+  # Test the helper methods
+  #
+
+  describe 'on_disk_configuration' do
     it 'returns nothing for an empty configuration' do
       expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return('')
-      config = provider.class.get_configuration
-      expect(config.size).to eq(0)
+      expect(provider.class).to receive(:ini_parse).with('').and_return('')
+      expect(provider.class.on_disk_configuration.size).to eq(0)
     end
-    it 'returns nothing for an garbage' do
+    it 'returns nothing for garbage' do
       expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return('asdlk;j12349567[[]]')
-      config = provider.class.get_configuration
-      expect(config.size).to eq(0)
+      expect(provider.class).to receive(:ini_parse).with('asdlk;j12349567[[]]').and_return('')
+      expect(provider.class.on_disk_configuration.size).to eq(0)
     end
     it 'returns as expected for example values' do
       expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(raw_data)
-      config = provider.class.get_configuration
-      expect(config.size).not_to eq(0)
+      expect(provider.class).to receive(:ini_parse).and_return(raw_values)
+      expect(provider.class).to receive(:repo_cert).with('/etc/rhsm/rhsm.conf').and_return('/etc/rhsm/ca/')
+      config = provider.class.on_disk_configuration
+      # expect(config.size).not_to eq(0)
       expect(config[:name]).to eq('/etc/rhsm/rhsm.conf')
       expect(config[:server_hostname]).to eq('katello.example.com')
       expect(config[:server_proxy_hostname]).to eq('proxy.example.com')
       expect(config[:provider]).to eq(:subscription_manager)
+      expect(config[:rhsm_repo_ca_cert]).to eq('/etc/rhsm/ca/')
     end
     it 'accepts hostnames with numbers in them' do
       expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(raw_hostname01_data)
-      config = provider.class.get_configuration
-      expect(config.size).not_to eq(0)
+      expect(provider.class).to receive(:ini_parse).and_return(raw_hostname01_values)
+      expect(provider.class).to receive(:repo_cert).with('/etc/rhsm/rhsm.conf').and_return('/etc/rhsm/ca/')
+      config = provider.class.on_disk_configuration
+      # expect(config.size).not_to eq(0)
       expect(config[:name]).to eq('/etc/rhsm/rhsm.conf')
       expect(config[:provider]).to eq(:subscription_manager)
       expect(config[:server_hostname]).to eq('katello01.example.com')
       expect(config[:rhsm_baseurl]).to eq('https://katello01.example.com/pulp/repos')
+      expect(config[:rhsm_repo_ca_cert]).to eq('/etc/rhsm/ca/')
+    end
+  end
+
+  describe 'repo_cert' do
+    it 'returns nothing for an empty configuration' do
+      expect(provider.class.repo_cert('')).to eq(nil)
+    end
+    it 'returns nothing for garbage' do
+      expect(provider.class.repo_cert('asdlk;j12349567[[]]')).to eq(nil)
+    end
+    it 'returns nothing if reading the file read fails' do
+      expect(File).to receive(:open).with('foo') { throw IOError }
+      expect(provider.class.repo_cert('foo')).to eq(nil)
+    end
+    it 'returns a reopsitory certificate' do
+      expect(File).to receive(:open).with('foo').and_return(raw_data)
+      expect(provider.class.repo_cert('foo')).to eq('/etc/rhsm/ca/')
+    end
+  end
+
+  describe 'ini_parse' do
+    it 'returns nothing for an empty configuration' do
+      expect(provider.class.ini_parse('')).to eq({})
+    end
+    it 'returns nothing for garbage' do
+      expect(provider.class.ini_parse('asdlk;j12349567[[]]')).to eq({})
     end
     properties.keys.each do |key|
-      next if key == :rhsm_repo_ca_cert
-      it "should parse the #{key} property" do
-        expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(raw_data)
-        configs = provider.class.instances
-        config = configs[0]
-        expect(config.public_send(key)).to eq(resource[key])
+      next if [:rhsm_repo_ca_cert, :name, :provider].include? key
+      it "parse the #{key} property" do
+        expect(provider.class.ini_parse(raw_data)[key]).to eq(raw_values[key])
       end
-    end
-    it 'parses the rhsm_repo_ca_cert property' do
-      expect(File).to receive(:open).with('/etc/rhsm/rhsm.conf').and_return(StringIO.new(raw_data))
-      expect(provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(raw_data)
-      configs = provider.class.instances
-      config = configs[0]
-      expect(config.public_send(:rhsm_repo_ca_cert)).to eq(resource[:rhsm_repo_ca_cert])
-    end
-    it 'detects default options' do
-      resource = Puppet::Type.type(:rhsm_config).new(
-        provider: provider, name: title,
-      )
-      expect(resource.provider.class).to receive(:subscription_manager).with(['config', '--list']).and_return(raw_hostname01_data)
-      config = resource.provider.class.get_configuration
-      expect(config.size).not_to eq(0)
-      expect(resource.provider.class.defaults_to?).to include(:server_port)
     end
   end
 
